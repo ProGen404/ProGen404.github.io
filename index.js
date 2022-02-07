5acbfe37bb6f7397b504e35a9e9af1a18cf8decf
Programming Student <mystiriodis13@gmail.com>
2022-02-07 21:50:14 +0200

    Now theme button changes text on toggle

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function darktheme() {
 	var element = document.body;
 	element.classList.toggle("dark-theme")
+
+	if (element.className == "dark-theme") {
+		document.getElementById('toggle').innerHTML = "Toggle Light Theme"
+	}
+	else {
+		document.getElementById('toggle').innerHTML = "Toggle Dark Theme"
+	}
 }
