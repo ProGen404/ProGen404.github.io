ed135e0ebec663086adb4dbe1288dc61b06bef70
ProGen404 <99261516+ProGen404@users.noreply.github.com>
2022-03-16 17:14:26 +0200

    Added image to the buttons instead
    Can I just automatically copy the other rep and paste it here?

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ function darktheme() {
 	var element = document.body;
 	element.classList.toggle("dark-theme")
 
-	if (element.className == "dark-theme") {
-		document.getElementById('toggle').innerHTML = "Toggle Light Theme"
+	if (element.classList.contains("dark-theme")) {
+		// document.getElementById('toggle').innerHTML = "Dark"
+		document.getElementById('togimg').src = "Icons/night.png"
 	}
 	else {
-		document.getElementById('toggle').innerHTML = "Toggle Dark Theme"
+		// document.getElementById('toggle').innerHTML = "Light"
+		document.getElementById('togimg').src = "Icons/day.png"
 	}
 }
